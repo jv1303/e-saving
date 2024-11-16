2f58b7511293d683dd122b8c274d7cd24ac8c26d
João Serafim <victorserafim2003@gmail.com>
2024-11-16 19:01:25 -0300

    index footer done

--- e-saving/wwwroot/js/indexFooter.js
+++ e-saving/wwwroot/js/indexFooter.js
@@ -3,5 +3,5 @@ console.log(currentUrl)
 const footerContainer = document.getElementById('footer-add');
 
 if (currentUrl == "/") {
-    footerContainer.innerHTML = "";
+    footerContainer.innerHTML = "<h3>Comece agora!</h3><div><a href='/User/Register' id='seja-parceiro-button'> Sing in </a><a href='/User/Login' id='seja-comprador-button'> Log in </a></div>";
 }
