789eeb4f8e3ad68f69f4b348e231def60cddaf73
Gustavo Pereira <adtpgugu@gmail.com>
2024-11-16 20:40:45 -0300

    mergeTest

---
+++ e-saving/Models/Cliente.cs
@@ -0,0 +1,20 @@
+namespace e_saving.Models 
+{
+
+public class Cliente 
+    {
+        public string CpfCliente {get; set;}
+        
+        public string NomeCliente {get; set;}
+        
+        public string SenhaCliente {get; set;}
+
+        public string EmailCliente {get; set;}
+
+        public byte[] ItensDescartados {get; set;} // cabível alterações
+
+        public
+
+    }
+
+}
