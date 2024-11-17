f50e3dad42b89a78fd3057458182139db1ae9b7f
João Serafim <victorserafim2003@gmail.com>
2024-11-16 21:39:24 -0300

    index page changes

--- e-saving/wwwroot/js/indexFooter.js
+++ e-saving/wwwroot/js/indexFooter.js
@@ -3,5 +3,5 @@ console.log(currentUrl)
 const footerContainer = document.getElementById('footer-add');
 
 if (currentUrl == "/") {
-    footerContainer.innerHTML = "<h3>Comece agora!</h3><div><a href='/User/Register' id='seja-parceiro-button'> Sing in </a><a href='/User/Login' id='seja-comprador-button'> Log in </a></div>";
+    footerContainer.innerHTML = "<h3>Comece agora!</h3><div><a href='/Home/SejaParceiro' id='seja-parceiro-button'> Torne-se parceiro </a><a href='/Home/SejaComprador' id='seja-comprador-button'> Torne-se comprador </a></div>";
 }
