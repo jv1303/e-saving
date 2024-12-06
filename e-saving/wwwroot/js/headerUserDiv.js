7e5c57b2d31d2a361553513e78e1de7696dd59f3
João Serafim <victorserafim2003@gmail.com>
2024-12-06 00:31:40 -0300

    user register page done

--- e-saving/wwwroot/js/headerUserDiv.js
+++ e-saving/wwwroot/js/headerUserDiv.js
@@ -16,5 +16,3 @@ if (userLogged == true) {
 } else {
     userDiv.innerHTML = "<a href='/User/Register' id='singin-button'> Sing in </a> <a href='/User/Login' id='login-button'> Log in </a>";  // puts the sing-in and log-in buttons into the HTML
 }
-
-
--- e-saving/wwwroot/js/register.js
+++ e-saving/wwwroot/js/register.js
@@ -1,15 +1,37 @@
-var inputCount;
+var inputCount, nextCount, isRegister
 const inputDiv = document.getElementsByClassName("user-input");
 const submit = document.getElementById("submit-button");
+const userForm = document.getElementById("user-form");
+const inputElement = document.getElementsByClassName("input-element");
+const emails = document.getElementsByName("email")
+const passwords = document.getElementsByName("password")
 
 function next() {
-    if (inputCount == 0) {
-        submit.type = "submit"
-    } else {
-        if (inputDiv[0].classList.includes("display-" + inputCount) == true) {
-            console.log("deu")
+    if (isRegister) {
+        if ((userForm.checkValidity() == false) || (emails[0].value != emails[1].value) || (passwords[0].value != passwords[1].value)) {
+            console.log(emails.value)
+            console.log(passwords.value)
+            alert("Preencha todos os campos corretamente para continuar.")
+            return
         }
     }
+
+    if (nextCount == 1) {
+        submit.value = "Cadastrar-se"
+    }
+    if (nextCount == 0) {
+        submit.type = "submit"
+    } else {
+        for (var i = inputCount; i > -1; i--) {
+            if (inputDiv[i].classList.contains("display-" + nextCount)) {
+                inputDiv[i].style.display = "flex"
+                inputElement[i].required = true
+            } else {
+                inputDiv[i].style.display = "none"
+            }
+        }
+    }
+    nextCount -= 1
 }
 
 console.log(inputDiv)
