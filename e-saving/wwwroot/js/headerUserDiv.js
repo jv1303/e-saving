272dab53380874e029e8d65fb9513a638a23b9c0
Gabriel Felix Pimenta <139069670+Gabriel-Felix-Pimenta@users.noreply.github.com>
2024-12-08 20:52:25 -0300

    alguma mensagem

--- e-saving/wwwroot/js/headerUserDiv.js
+++ e-saving/wwwroot/js/headerUserDiv.js
@@ -17,4 +17,34 @@ if (userLogged == true) {
     userDiv.innerHTML = "<a href='/User/Register' id='singin-button'> Sing in </a> <a href='/User/Login' id='login-button'> Log in </a>";  // puts the sing-in and log-in buttons into the HTML
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
 
+    // Alterna a visibilidade do menu ao clicar no botão hambúrguer
+    menuToggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
+
+    // Manipula o estado de login do usuário
+    var userLogged = false; // Simula o estado de login do usuário
+    const userDiv = document.getElementById('user-div');
+    var userName;
+
+    if (userLogged) {
+        userName = "Nome de usuário"; // Nome fictício do usuário
+
+        userDiv.innerHTML = `
+            <div id="logged-user-click">
+                <h6 id="user-name">
+                    ${userName}
+                    <img src="/images/down-arrow.png" id="user-down-arrow" alt="Seta para baixo">
+                </h6>
+                <img src="/images/user-icon.png" alt="Ícone de usuário" class="header-icon">
+            </div>`;
+    } else {
+        userDiv.innerHTML = `
+            <a href="/User/Register" id="singin-button">Sign in</a>
+            <a href="/User/Login" id="login-button">Log in</a>`;
+    }
+});
